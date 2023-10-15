7d6d2f2f863492b8a2b1013cf22e2044238b2baa
Robert Garcia <robertserranogar@gmail.com>
2023-10-15 08:26:15 -0600

    new comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+// import "./styles.css";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState("");
+
+    const addTask = () => {
+        if (newTask.trim() !== "") {
+            setTasks([...tasks, newTask]);
+            setNewTask("");
+        }
+    };
+
+    const removeTask = (index) => {
+        const updatedTasks = [...tasks];
+        //splice, looks for index, then the delete count
+        updatedTasks.splice(index, 1);
+        setTasks(updatedTasks);
+    };
+
+    return (
+        <div className="App">
+            <div className="description">
+                <h3>To Do List</h3>
+                <h4>Instructions</h4>
+                <p>
+                    To add a new task: You can either click the button or click
+                    enter on your keyboard
+                </p>
+                <p>
+                    To delete a task: double click on any task you would like to
+                    delete
+                </p>
+            </div>
+            <div className="container">
+                <div className="input-wrap">
+                    <input
+                        placeholder="add a task"
+                        value={newTask}
+                        onChange={(e) => setNewTask(e.target.value)}
+                    ></input>
+                    <button onClick={addTask}>Add task</button>
+                </div>
+                <div className="tasks-wrap">
+                    <ul>
+                        {tasks.map((task, index) => (
+                            <li
+                                key={index}
+                                onDoubleClick={() => removeTask(index)}
+                            >
+                                {task}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default App;
